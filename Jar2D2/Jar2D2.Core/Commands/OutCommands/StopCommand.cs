6ae8ae49b7b11671acdfe40a150b10d04afa7ff6
AJ <andrew@jobadder.com>
2019-12-13 13:15:53 +1100

    Created a shell turning command for the wheels

--- Jar2D2/Jar2D2.Core/Commands/OutCommands/StopCommand.cs
+++ Jar2D2/Jar2D2.Core/Commands/OutCommands/StopCommand.cs
@@ -1,19 +1,20 @@
 ﻿using ArduinoDriver.SerialProtocol;
+using Jar2D2.Core.ArduinoWrapper;
 using System.Collections.Generic;
 
 namespace Jar2D2.Core.Commands.OutCommands
 {
-	//public class StopCommand : IOutCommand
-	//{
- //   public IDictionary<int, int> CommandRegistry { get; set; }
+  public class StopCommand : IOutCommand
+  {
+    public IDictionary<int, int> CommandRegistry { get; set; }
 
- //   public void Execute(ArduinoDriver.ArduinoDriver driver)
- //   {
- //     var leftWheel = new DigitalWriteRequest(1, ArduinoDriver.DigitalValue.Low);
- //     var rightWheel = new DigitalWriteRequest(2, ArduinoDriver.DigitalValue.Low);
+    public void Execute(IArduino arduino)
+    {
+      var leftWheel = new DigitalWriteRequest(1, ArduinoDriver.DigitalValue.Low);
+      var rightWheel = new DigitalWriteRequest(2, ArduinoDriver.DigitalValue.Low);
 
- //     driver.Send(rightWheel);
- //     driver.Send(leftWheel);
- //   }
- // }
+      arduino.Send(rightWheel);
+      arduino.Send(leftWheel);
+    }
+  }
 }
---
+++ Jar2D2/Jar2D2.Core/Commands/OutCommands/TurnLeft10DegreesCommand.cs
@@ -0,0 +1,37 @@
+﻿using ArduinoDriver.SerialProtocol;
+using Jar2D2.Core.ArduinoWrapper;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Jar2D2.Core.Commands.OutCommands
+{
+  public class TurnLeft10DegreesCommand : IOutCommand
+  {
+    public IDictionary<int, int> CommandRegistry { get; set; }
+
+    public async Task Execute(IArduino arduino)
+    {
+      var leftWheelOn = new DigitalWriteRequest(1, ArduinoDriver.DigitalValue.High);
+      var leftWheelOff = new DigitalWriteRequest(1, ArduinoDriver.DigitalValue.Low);
+
+      var rightWheelOn = new DigitalWriteRequest(2, ArduinoDriver.DigitalValue.High);
+      var rightWheelOff = new DigitalWriteRequest(2, ArduinoDriver.DigitalValue.Low);
+
+      Task delay = Task.Delay(2000);
+
+      Task task1 = new Task(async () => {
+        arduino.Send(leftWheelOff);
+        arduino.Send(rightWheelOn);
+      });
+
+      Task task2 = new Task(async () => {
+        arduino.Send(leftWheelOn);
+        arduino.Send(rightWheelOn);
+      });
+
+      await task1;
+      await delay;
+      await task2;
+    }
+  }
+}
